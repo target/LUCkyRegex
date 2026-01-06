b61026428ef6fa25d483000c0abc5daaba8bf68e
Mauricio Rossi Sabarros <mauricio.rossisabarros@target.com>
2026-01-06 13:58:32 -0600

    refactor: package updates across the board

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ rm -r dotnet-service/bin/
 cd dotnet-service
 dotnet publish -c Release
 cd ../
-rm dotnet-service/bin/Release/net7.0/browser-wasm/AppBundle/index.html
+rm dotnet-service/bin/Release/net9.0/browser-wasm/AppBundle/index.html
 yarn run build
-cp -r dotnet-service/bin/Release/net7.0/browser-wasm/AppBundle/* ./docs
+cp -r dotnet-service/bin/Release/net9.0/browser-wasm/AppBundle/* ./docs
 touch ./docs/.nojekyll # The _ in _next causes issues with GH Pages
--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ cd dotnet-service
 dotnet publish -c Release
 cd ../
 mkdir public
-cp -r dotnet-service/bin/Release/net7.0/browser-wasm/AppBundle/* public
+cp -r dotnet-service/bin/Release/net9.0/browser-wasm/AppBundle/* public
 yarn run next:dev
