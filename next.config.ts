42b05525ac54915895b034a1fcb92349481ba4c2
Mauricio Rossi Sabarros <mauricio.rossisabarros@target.com>
2026-01-07 10:08:10 -0600

    fix: next.config path

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: "export",
+  distDir: "docs",
+  assetPrefix: "./",
 };
 
 export default nextConfig;
