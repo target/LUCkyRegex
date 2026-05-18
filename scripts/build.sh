0daee2fc6d7b2814bc83ed16f5ddd84eabd19f51
Josh Gossett <Josh.Gossett@target.com>
2026-05-18 08:58:14 -0500

    feat(tool): remove vitest.config.mts from typescript build

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@ rm -r docs
 rm -r dotnet-service/bin/
 cd dotnet-service
 dotnet publish -c Release
-cd ../
+cd ..
 rm dotnet-service/bin/Release/net9.0/browser-wasm/AppBundle/index.html
 npm run next:build
 mkdir ./docs/LuceneNet
